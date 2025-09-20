47ab7b5c606a9d2ec93f4bf3ddbd41dce76b1aa0
HSevenn <91921247+HSevenn@users.noreply.github.com>
2025-09-20 16:00:28 -0500

    Update supabase-browser.ts

--- lib/supabase-browser.ts
+++ lib/supabase-browser.ts
@@ -1,10 +1,16 @@
 'use client';
-
-import { createBrowserClient } from '@supabase/ssr';
+import { createClient } from '@supabase/supabase-js';
 
 export function supabaseBrowser() {
-  return createBrowserClient(
+  return createClient(
     process.env.NEXT_PUBLIC_SUPABASE_URL!,
-    process.env.NEXT_PUBLIC_SUPABASE_ANON_KEY!
+    process.env.NEXT_PUBLIC_SUPABASE_ANON_KEY!,
+    {
+      auth: {
+        persistSession: true,
+        autoRefreshToken: true,
+        detectSessionInUrl: false, // no usamos magic link
+      },
+    }
   );
 }
