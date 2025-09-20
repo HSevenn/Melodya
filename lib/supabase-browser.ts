b1ae7f2135e72fb38087b73804faac420e9560ac
HSevenn <91921247+HSevenn@users.noreply.github.com>
2025-09-20 13:59:42 -0500

    Update supabase-browser.ts

--- lib/supabase-browser.ts
+++ lib/supabase-browser.ts
@@ -2,9 +2,7 @@
 
 import { createBrowserClient } from '@supabase/ssr';
 
-export function supabaseBrowser() {
-  return createBrowserClient(
-    process.env.NEXT_PUBLIC_SUPABASE_URL!,
-    process.env.NEXT_PUBLIC_SUPABASE_ANON_KEY!
-  );
-}
+export const supabase = createBrowserClient(
+  process.env.NEXT_PUBLIC_SUPABASE_URL!,
+  process.env.NEXT_PUBLIC_SUPABASE_ANON_KEY!
+);
